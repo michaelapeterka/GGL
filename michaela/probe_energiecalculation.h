5aceca10c316e03d205d77165b888976aa93a6dd
Michaela Peterka <michaela.peterka@gmx.at>
2017-12-06 15:04:21 +0100

    header file energieberechnung

---
+++ michaela/probe_energiecalculation.h
@@ -0,0 +1,14 @@
+#include <openbabel/obconversion.h>
+#include <openbabel/mol.h>
+#include <openbabel/forcefield.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <utility>
+
+using namespace std;
+
+
+double energie_calculation(vector<string>&);
