c9fdcb3c18c23bc560eea0236bc9f8d0bfe1b706
Michaela Peterka <michaela.peterka@gmx.at>
2017-12-12 14:38:36 +0100

    new version probe_energiecalculation.h

--- michaela/probe_energiecalculation.h
+++ michaela/probe_energiecalculation.h
@@ -3,12 +3,18 @@
 #include <openbabel/forcefield.h>
 
 #include <iostream>
+#ifndef PROBE_ENERGIECALCULATION_H
+#define PROBE_ENERGIECALCULATION_H
+
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <utility>
+#include <map>
 
 using namespace std;
 
 
-double energie_calculation(vector<string>&);
+double energie_calculation(std::map<std::string, double>&);
+
+#endif
