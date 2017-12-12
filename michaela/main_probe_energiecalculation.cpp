c578e3b68f6034fce0237fda0c15bd623b76c20f
Michaela Peterka <michaela.peterka@gmx.at>
2017-12-12 14:37:20 +0100

    new version main_probe_energiecalculation.cpp

--- michaela/main_probe_energiecalculation.cpp
+++ michaela/main_probe_energiecalculation.cpp
@@ -8,18 +8,32 @@
 #include <sstream>
 #include <utility>
 #include "probe_energiecalculation.h"
+#include <map>
 
 using namespace std;
 
 int main()
 {
-vector<string> probe_smiles;
+  //Probeversion/Probewerte etc.
+  std::map<std::string,double> probe_smiles;
+  std::map<std::string,double> metabolites;
+  std::string smiles = "[H:1][C:1]#[N:1]";
+  std::string m_smiles = "[H:1]";
+  std::string m_smiles1 = "[C:1][N:1]";
 
-probe_smiles.push_back("[H:1][C:1]#[N:1]");
-
+  metabolites[m_smiles]= 54.0;
+  probe_smiles[smiles] = 5.6978;
+  metabolites[m_smiles1] = 4565.09; 
+  
+  
 double energie_producedSmiles = energie_calculation(probe_smiles);
+double energie_metabolites = energie_calculation(metabolites);
+ double a0 = energie_producedSmiles - energie_metabolites;
+cout << "Energy_producedSmiles:" << energie_producedSmiles << endl;
+ cout << "Energy_metabolites: " << energie_metabolites << endl;
+ cout <<"a0 = " << a0 << endl;
 
-cout << "Energy_producedSmiles" << energie_producedSmiles << endl; 
+ 
 return 0;
 }
 
